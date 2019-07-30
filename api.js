e0131f25fb46e144bb786414de2207a717635b79
BILL-E_MAGNUS\Christopher <christopher.aranadi@mail.utoronto.ca>
2019-07-29 20:03:05 -0400

    Add redirect for /

--- api.js
+++ api.js
@@ -2,6 +2,10 @@ const express = require('express');
 const missa = require('./routes/missa');
 const app = express();
 
+app.get('/', (req, res) => {
+    res.redirect('/missa');
+})
+
 app.use('/missa', missa)
 
 app.listen(process.env.PORT || 8000, () => {
