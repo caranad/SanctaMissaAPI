56aefe3736c8b16eaaaade26f0800bdb3a415fa2
Christopher Aranadi <christopher.aranadi@mail.utoronto.ca>
2020-10-05 22:01:10 -0400

    Use recently created npm module for sancta-missa

--- routes/missa.js
+++ routes/missa.js
@@ -1,36 +1,39 @@
 const express = require('express');
-const sanctamissa = require('../index/index');
+const sanctamissa = require('sancta-missa');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     const month = new Date().getMonth() + 1;
     const day = new Date().getDate();
     const year = new Date().getFullYear();
 
-    sanctamissa.getMassData(res, month, day, year);
+    const response = await sanctamissa(month, day, year);
+    res.json(JSON.parse(response));
 })
 
 /**
  * Get the Mass propers for month M, day D
  */
-router.get('/:month/:day', (req, res) => {
+router.get('/:month/:day', async (req, res) => {
     const month = req.params.month;
     const day = req.params.day;
     const year = new Date().getFullYear();
 
-    sanctamissa.getMassData(res, month, day, year);
+    const response = await sanctamissa(month, day, year);
+    res.json(JSON.parse(response));
 })
 
 /**
  * Get the Mass propers for month M, day D, year Y
  */
-router.get('/:month/:day/:year/:num?', (req, res) => {
+router.get('/:month/:day/:year/:num?', async (req, res) => {
     const month = req.params.month;
     const day = req.params.day;
     const year = req.params.year;
     const num = req.params.num;
 
-    sanctamissa.getMassData(res, month, day, year, num);
+    const response = await sanctamissa(month, day, year, num);
+    res.json(JSON.parse(response));
 })
 
 module.exports = router;
